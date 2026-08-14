dfff000a9e11dc6f0b2444da9770735f903384a7
kaito <K.Bolliger.inf24@stud.bbbaden.ch>
2026-08-14 11:23:26 +0200

    Add files via upload

---
+++ GamePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BlackJack;
+
+public partial class GamePage : ContentPage
+{
+    public GamePage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace BlackJack;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnPlayClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GamePage());
+    }
+}
