a784134f88bd77519b8eccbc449b4fb9491380bf
kaito <K.Bolliger.inf24@stud.bbbaden.ch>
2026-08-21 11:37:24 +0200

    Add files via upload

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -2,8 +2,34 @@ namespace BlackJack;
 
 public partial class GamePage : ContentPage
 {
+    int score; 
+    Random random = new Random();
     public GamePage()
     {
         InitializeComponent();
+
+
+
+        int firstCard = 7;
+        int secondCard = 10;
+        score = firstCard + secondCard;
+
+        PlayerScoreLabel.Text = score.ToString();
     }
+
+
+    private void OnHitClicked(object sender, EventArgs e)
+    {
+        int newCard = random.Next(2, 11);
+        score = score + newCard;
+        PlayerScoreLabel.Text = score.ToString();
+    }
+
+
+
+    private void OnStandClicked(object sender, EventArgs e)
+    {
+
+    }
+
 }
